a0dcc5deaf9929fae245b13550b53d8ee92b722a
Whiplashhh <willem.vanbaelinghem@proton.me>
2026-05-04 16:25:52 +0200

    tests

---
+++ tests/none.test.ts
@@ -0,0 +1,6 @@
+import { describe, it, expect } from 'vitest'
+describe('Vitest Setup', () => {
+  it('First Test only for setup', async () => {
+    expect(1).toBe(1)
+  })
+})
--- vitest.config.js
+++ vitest.config.js
@@ -1,24 +1,24 @@
-import {defineConfig} from 'vitest/config'
+import { defineConfig } from 'vitest/config'
 import path from 'path'
 export default defineConfig({
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
-        },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
-    test: {
-        globals: true,
-        environment: 'node',
-        coverage: {
-            provider: 'v8',
-            reporter: ['text', 'json', 'html'],
-            exclude: [
-                'node_modules/',
-                'dist/',
-                '**/*.config.ts',
-                '**/*.test.ts',
-                '**/*.spec.ts',
-            ],
-        },
+  },
+  test: {
+    globals: true,
+    environment: 'node',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'node_modules/',
+        'dist/',
+        '**/*.config.ts',
+        '**/*.test.ts',
+        '**/*.spec.ts',
+      ],
     },
+  },
 })
